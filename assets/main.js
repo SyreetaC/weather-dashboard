4147b1b212ea26be47d3b1aa8ee29a9706e9bf4f
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-04-08 11:25:19 +0100

    pseudo code added

--- assets/main.js
+++ assets/main.js
@@ -1,35 +1,106 @@
-//need click function for submit button
-//need function for requesting both current weather and 5 day weather.- both with a url passed in
-//handle errors function
-//function for JSON
-const requestServerData = () => {
-  const functionForJSON = (responseObject) => {
+const renderCities = (citiesFromLocalStorage) => {
+  //for each city, construct a list item and append to the list group.
+};
+
+const getCurrentData = (oneApiData) => {
+  //from object, extract the data points you need for the return data
+  return {
+    name: "",
+    date: "",
+    iconURL: "",
+    temperature: "",
+    humidity: "",
+    windSpeed: "",
+    uvIndex: 0,
+  };
+};
+
+const getForecastData = (oneApiData) => {
+  //iterate and construct the return data array
+  return [
+    {
+      date: "",
+      iconURL: "",
+      temperature: "",
+      humidity: "",
+    },
+  ];
+};
+renderCurrentCardComponent = (currentData) => {};
+renderForecastComponents = (forecastData) => {};
+
+const fetchAllWeatherData = (cityName) => {
+  const weatherApiUrl = fetch(
+    "http://api.openweathermap.org/data/2.5/forecast?q={cityname}&appid=d34a16a53b39e4eaf211ff76b0adf70c"
+  );
+  //construct URL for weather URL and store in variable called weatherApiUrl
+
+  const functionForJson = (responseObject) => {
     if (responseObject.status !== 200) {
       throw new Error("Internal Server Error");
+    } else {
+      return responseObject.json();
     }
-    return responseObject.json();
-    // unless you have some logic here do that before you return
   };
   const functionForApplication = (dataFromServer) => {
     console.log(dataFromServer);
-    // whatever your application code is goes here
+    //whatever your application code is goes here
+    //1. from the datafromserver get the lat and long
+    //2. use lat and long to contruct next URL with 5 day weather and store in variable called oneCallApiUrl
+    //once you have the next URL:
+    const functionForJSON = (responseObject) => {
+      // unless you have some logic here do that before you return
+      return responseObject.json();
+    };
+    const functionForApplication = (dataFromServer) => {
+      const oneApiData = dataFromServer;
+      //take current day data and forecast data from here by calling a function for each
+      const currentData = getCurrentData();
+      const forecastData = getForecastData();
+
+      renderCurrentCardComponent();
+      renderForecastComponents();
+      // whatever your application code is goes here
+    };
+    const functionToHandleError = (errorObject) => {
+      // handle your error here according to your application
+    };
+    fetch(oneCallApiUrl)
+      .then(functionForJSON)
+      .then(functionForApplication)
+      .catch(functionToHandleError);
   };
+
   const functionToHandleError = (errorObject) => {
     console.log("Something went wrong");
     // handle your error here according to your application
   };
-  fetch(
-    "http://api.openweathermap.org/data/2.5/forecast?q={cityname}&appid=d34a16a53b39e4eaf211ff76b0adf70c"
-  )
+
+  fetch(weatherApiUrl)
     .then(functionForJSON)
     .then(functionForApplication)
     .catch(functionToHandleError);
 };
 
-const requestCurrentWeather = () => {
-  const currentWeatherURL =
-    "http://api.openweathermap.org/data/2.5/weather?q={cityname}&appid=d34a16a53b39e4eaf211ff76b0adf70c";
-  requestServerData();
+const onLoad = () => {
+  //read from local storage and store data in variable called cities from local storage
+  //if data is present, call rendercities and pass the data from local storage
+  //render cities function (cities from local storage)
+  //get the last city name from citiesfromlocalstorage and store in a variable called cityname
+  //fetch weather data(city name)
 };
 
-$(document).ready(requestServerData);
+//need click function for submit button
+
+const onSubmit = (cityName) => {
+  //get city name and store as variable
+  fetchAllWeatherData(cityName);
+};
+
+const onClick = () => {
+  //get city name from the list item that was clicked and store as variable
+  fetchAllWeatherData(cityName);
+};
+$("#input-form").submit(onSubmit);
+$("#constructed-list-items").click(onClick);
+$(document).ready(onLoad);
